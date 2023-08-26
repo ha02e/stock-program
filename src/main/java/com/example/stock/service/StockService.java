1bfbec6362c0b9427cfc8f5870f71e988650a236
HAYOUNG <ha02e@naver.com>
2023-08-26 19:44:53 +0900

    230826_propagation 변경

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,8 +3,9 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @Service
 public class StockService {
@@ -15,8 +16,9 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    //@Transactional
+
     //재고 감소 메서드
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public synchronized void decrease(Long id, Long quantity){
         // Stock 조회
         Stock stock = stockRepository.findById(id).orElseThrow();

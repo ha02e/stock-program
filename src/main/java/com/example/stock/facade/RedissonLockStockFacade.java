46bc22a3caa1ea26a504a55db540fcdee20887c7
HAYOUNG <ha02e@naver.com>
2023-09-15 16:57:15 +0900

    230915_Redisson을 활용해 재고감소 로직 작성

---
+++ src/main/java/com/example/stock/facade/RedissonLockStockFacade.java
@@ -0,0 +1,44 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.StockService;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedissonLockStockFacade {
+
+    private RedissonClient redissonClient;
+
+    private StockService stockService;
+
+    public RedissonLockStockFacade(RedissonClient redissonClient, StockService stockService) {
+        this.redissonClient = redissonClient;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long id, Long quantity){
+        //lock 객체 가져오기
+        RLock lock = redissonClient.getLock(id.toString());
+
+        try {
+            boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+            //lock 획득 실패 시
+            if(!available){
+                System.out.println("lock 획득 실패");
+                return;
+            }
+
+            //lock 획득 성공 시
+            stockService.decrease(id,quantity);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }finally {
+            lock.unlock();
+        }
+    }
+
+}
